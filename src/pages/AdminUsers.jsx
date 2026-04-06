5b5b6b28dee89d088a63fccd994c2c1549b436fd
Carsten Barth <carsten.barth@me.com>
2026-04-06 22:45:34 +0000

    Admin-Seitenstruktur und Navigation

--- src/pages/AdminUsers.jsx
+++ src/pages/AdminUsers.jsx
@@ -145,7 +145,7 @@ export default function AdminUsers() {
             </Button>
           </Link>
           <Button onClick={() => { setInviteEmail(""); setInviteRole("user"); }}>
-            <UserPlus className="h-4 w-4 mr-2" /> Nutzer ({users.length})
+            <UserPlus className="h-4 w-4 mr-2" /> Nutzer einladen
           </Button>
         </div>
       </div>
