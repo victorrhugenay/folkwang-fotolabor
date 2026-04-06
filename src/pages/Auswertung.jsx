e62dec1f87070e7e5f8e994175a60ff60f354d56
Carsten Barth <carsten.barth@me.com>
2026-04-06 08:54:41 +0000

    File changes

--- src/pages/Auswertung.jsx
+++ src/pages/Auswertung.jsx
@@ -24,7 +24,7 @@ export default function Auswertung() {
   }
 
   useEffect(() => {
-    if (userLoading) return;
+    if (userLoading || !isAdmin) return;
     
     Promise.all([
       base44.entities.User.list(),
