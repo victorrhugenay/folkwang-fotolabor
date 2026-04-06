fc32b63a01b0d2e32ac9e68050f492bc2e2ddb54
Carsten Barth <carsten.barth@me.com>
2026-04-06 17:17:58 +0000

    File changes

--- src/pages/Materials.jsx
+++ src/pages/Materials.jsx
@@ -333,10 +333,6 @@ function MaterialFormDialog({ open, onOpenChange, item, onSave }) {
         </DialogHeader>
         <div className="space-y-4 py-2">
           <div>
-            <Label>Bild</Label>
-            <ImageUpload value={form.image_url || ""} onChange={url => setForm(f => ({ ...f, image_url: url }))} />
-          </div>
-          <div>
             <Label>Name *</Label>
             <Input value={form.name || ""} onChange={e => setForm({ ...form, name: e.target.value })} />
           </div>
